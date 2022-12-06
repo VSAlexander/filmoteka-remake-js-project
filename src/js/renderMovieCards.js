850ae8102548ebf903c6e40979697c40df04da80
VSAlexander <shevchenkoav.97@gmail.com>
2022-12-06 10:57:54 +0200

    fixed cards markup and added new movie-cards html to partials

--- src/js/renderMovieCards.js
+++ src/js/renderMovieCards.js
@@ -4,28 +4,39 @@ export function renderMovieCards(data) {
   return data
     .map(
       movie => `<li class="movies-list__item">
+      <div class="movies-list__item-thumb">
         <img loading="lazy"
           class="movies-list__item-card-img"
           src="https://image.tmdb.org/t/p/w342${movie.poster_path}"
           srcset="https://image.tmdb.org/t/p/w342${movie.poster_path} 1x,
-          https://image.tmdb.org/t/p/w500${movie.poster_path} 2x"
+          https://image.tmdb.org/t/p/w780${movie.poster_path} 2x"
 
           alt="${movie.title}">
-          
+      </div>
         <div class="movies-list__item-caption">
             <h2 class="movies-list__item-title">${movie.title}</h2>
-            <p class="movies-list__item-info">${getGenresFromLocalStorage(
-              'genres'
-            )
-              .filter(element => movie.genre_ids.includes(element.id))
-              .map(element => element.name)
-              .join(', ')} | ${
+            <p class="movies-list__item-info">${checkLengthOfGenres(
+              movie.genre_ids
+            )}┃${
         movie.release_date.slice(0, 4)
           ? movie.release_date.slice(0, 4)
-          : 'No information about release date'
+          : 'No info'
       }</p>
         </div>
         </li>`
     )
     .join('');
+}
+
+function checkLengthOfGenres(array) {
+  const allGenres = getGenresFromLocalStorage('genres')
+    .filter(genre => array.includes(genre.id))
+    .map(genre => genre.name);
+
+  if (allGenres.length > 3) {
+    allGenres.splice(2, 1, 'Other');
+    return allGenres.slice(0, 3).join(', ');
+  } else {
+    return allGenres.slice(0, 3).join(', ');
+  }
 }
