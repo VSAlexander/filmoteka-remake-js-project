7fbd34206a47f5bed119d27cf4cb72d85bbb492c
Serhii26 <25kontrakt@gmail.com>
2022-12-07 16:43:04 +0200

    commit setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { changeTheme } from './js/changeTheme';
  import { spinner } from './js/spinner';
 import { renderMovieCards } from './js/renderMovieCards';
  import { fetchGenres, getGenresFromLocalStorage, API_KEY } from './js/fetchGenres'; 
-//   import { render, renderOnSearchMistake } from './js/cards';
+//    import { render, renderOnSearchMistake } from './js/cards';
  
 
