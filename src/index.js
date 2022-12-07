8e6cc8735dd2434d9b20b5fc1fb908b786903f70
Serhii26 <25kontrakt@gmail.com>
2022-12-07 11:58:56 +0200

    commit setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import { search } from './js/search';
-// import { changeTheme } from './js/changeTheme';
+import { searchMovie } from './js/search';
+import { changeTheme } from './js/changeTheme';
