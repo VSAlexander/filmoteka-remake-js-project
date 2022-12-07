8bdc7294427e4b3d2a03da76b0a2599ba2e35ae0
Serhii26 <25kontrakt@gmail.com>
2022-12-07 20:53:42 +0200

    comit setup

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import { team } from './js/team';
 // import { searchMovie } from './js/search';
 // import  changeTheme  from './js/changeTheme';
 import { spinner } from './js/spinner';
-import { renderMovieCards } from './js/renderMovieCards';
+// import { renderMovieCards } from './js/renderMovieCards';
 
 // //    import { render, renderOnSearchMistake } from './js/cards';
-import { getMovies } from './js/fetchTrendingMovies';
+// import { getMovies } from './js/fetchTrendingMovies';
 // import { search } from './js/search';
 import { changeTheme } from './js/changeTheme';
 import { team } from './js/team';
 // import { searchMovie } from './js/search';
 import { changeTheme } from './js/changeTheme';
 import { spinner } from './js/spinner';
-import { renderMovieCards } from './js/renderMovieCards';
+// import { renderMovieCards } from './js/renderMovieCards';
 import {
   fetchGenres,
   getGenresFromLocalStorage,
   API_KEY,
-} from './js/fetchGenres';
+} from './js/fetch-and-render-trending';
 //    import { render, renderOnSearchMistake } from './js/cards';
 
